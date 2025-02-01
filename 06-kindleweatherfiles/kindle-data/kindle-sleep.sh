b000e01ce055a9d470f3911a0c4a6e8329513674
james <git@mus-ic.co.uk>
2025-02-01 14:06:56 +0000

    rotate the picture and fix the location of it in the sleep script

--- 06-kindleweatherfiles/kindle-data/kindle-sleep.sh
+++ 06-kindleweatherfiles/kindle-data/kindle-sleep.sh
@@ -7,4 +7,4 @@ stop framework
 lipc-set-prop -i com.lab126.powerd preventScreenSaver 1
 lipc-set-prop com.lab126.pillow disableEnablePillow disable
 
-eips -f -g sleeping-670-1026.png
+eips -f -g /mnt/us/kindle-data/sleeping-670-1026.png
