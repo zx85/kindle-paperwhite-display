0803811246cb6052eef79181195c6a8c31945e9b
james@pinfinity <github@mus-ic.co.uk>
2025-05-28 15:32:14 +0100

    remove kindle battery if no value

--- kindleDisplay/includes/kindle_battery.py
+++ kindleDisplay/includes/kindle_battery.py
@@ -1,7 +1,15 @@
+import logging
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)  # Set the default logging level to DEBUG
+
+
 def display_kindle_battery(kindle_battery, display):  # Kindle battery
-    display.draw.text(
-        (960, 715),
-        f"{kindle_battery}%",
-        font=display.suffix_font,
-        fill=(0),
-    )
+    log.debug(f"Kindle battery: {kindle_battery}")
+    if kindle_battery:
+        display.draw.text(
+            (960, 715),
+            f"{kindle_battery}%",
+            font=display.suffix_font,
+            fill=(0),
+        )
