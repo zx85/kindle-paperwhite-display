c2f9958e3b636740e784e1996226870353b1097e
james <git@mus-ic.co.uk>
2026-01-30 19:31:33 +0000

    change the URL

--- 06-kindleweatherfiles/kindle-data/kindle-data.sh
+++ 06-kindleweatherfiles/kindle-data/kindle-data.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# Endpoint hosting the picture
+url="http://192.168.75.4:5050"
 
 cd "$(dirname "$0")"
 rm kindle-data.png
@@ -10,11 +12,11 @@ lipc-set-prop com.lab126.pillow disableEnablePillow disable
 
 battery_level=$(lipc-get-prop -i com.lab126.powerd battLevel)
 
-if wget -O kindle-data.png "http://192.168.75.4:5050/kindle-data.png?battery=${battery_level}"; then
+if wget -O kindle-data.png "${url}/kindle-data.png?battery=${battery_level}"; then
         eips -f -g kindle-data.png
 else
         sleep 10
-        if wget -O kindle-data.png "http://192.168.75.4:5050/kindle-data.png?battery=${battery_level}"; then
+        if wget -O kindle-data.png "${url}/kindle-data.png?battery=${battery_level}"; then
         eips -f -g kindle-data.png
         fi
 
